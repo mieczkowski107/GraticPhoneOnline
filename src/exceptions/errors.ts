9c9fae2848e3507aed617ecbb3c88a1894de08d2
Łukasz Mieczkowski <89240@student.pb.edu.pl>
2025-11-11 17:27:24 +0100

    Add custom error classes for improved error handling

---
+++ src/exceptions/errors.ts
@@ -0,0 +1,22 @@
+export class NotFoundError extends Error {
+    constructor(msg: string) {
+        super(msg);
+        this.name = "InvalidError";
+        Object.setPrototypeOf(this, InvalidInputError.prototype);
+    }
+}
+export class InvalidInputError extends Error {
+    constructor(msg: string) {
+        super(msg);
+        this.name = "InvalidError";
+        Object.setPrototypeOf(this, InvalidInputError.prototype);
+    }
+}
+
+export class AuthenticationError extends Error {
+    constructor(msg: string) {
+        super(msg);
+        this.name = "AuthenticationError";
+        Object.setPrototypeOf(this, AuthenticationError.prototype);
+    }
+}
